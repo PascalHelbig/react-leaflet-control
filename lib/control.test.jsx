8a74225848a59a43c4feb3b8933f72390c2dcaff
PascalHelbig <pascalhelbig@gmail.com>
2017-11-17 11:36:24 +0100

    add simple unit test

---
+++ lib/control.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import { Map } from 'react-leaflet';
+import Control from './control';
+
+it('renders without errors', () => {
+  const wrapper = mount(
+    <Map>
+      <Control position="topleft" >
+        <div>Test</div>
+      </Control>
+    </Map>
+  );
+});
